3e8b2a5cc7fb71d5ef24ca1f3fefd460c379bc9d
zuzzorellone <christian.esposito96@gmail.com>
2023-05-26 15:52:42 +0200

    Realized Parallax backgrounds

--- animation/animation.js
+++ animation/animation.js
@@ -1,3 +1,9 @@
+let animationType = "idle";
+const dropDown = document.getElementById("animations");
+dropDown.addEventListener("change", function (e) {
+    animationType = e.target.value;
+});
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 console.log(ctx);
@@ -14,8 +20,6 @@ playerImage.src = 'shadow_dog.png';
 const spriteWidth = 575;
 const spriteHeight = 523;
 
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrame = 3;
 
@@ -29,6 +33,39 @@ const animationStates = [
         name: "jump",
         frames: 7,
     },
+    {
+        name: "fall",
+        frames: 7,
+    },
+    {
+        name: "run",
+        frames: 9,
+    },
+    {
+        name: "dizzy",
+        frames: 11,
+    },
+    {
+        name: "sit",
+        frames: 5,
+    },
+    {
+        name: "roll",
+        frames: 7,
+    },
+    {
+        name: "bite",
+        frames: 7,
+    },
+    {
+        name: "ko",
+        frames: 12,
+    },
+    {
+        name: "getHit",
+        frames: 4,
+    },
+
 ];
 
 // SCORRO LE VARIE ANIMAZIONI POSSIBILI
@@ -53,14 +90,15 @@ function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
     // CALCOLO LA POSIZIONE DEL PROSSIMO FRAME DELL'ANIMAZIONE DA DISEGNARE
-    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimation["idle"].loc.length;
-    frameX = spriteWidth * position;
+    let position = Math.floor(gameFrame/staggerFrame) % spriteAnimation[animationType].loc.length;
+    let frameX = spriteWidth * position;
+    let frameY = spriteAnimation[animationType].loc[position].y;
 
     // DISEGNO LA MIA IMMAGINE
     // drawImage(nomeImg, coordinataX da cui ritagliare, cooY da cui ritagliare, larghezza da ritagliare
     // altezza da ritagliate, cooX del canvas su cui metterel'img, cooY del canvas su cui mettere l'img,
     // larghezza della immagine, altezza della immagina)
-    ctx.drawImage(playerImage, frameX, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+    ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
     gameFrame++;
     requestAnimationFrame(animate);
 }
---
+++ background/background.js
@@ -0,0 +1,77 @@
+const canvas = document.getElementById("canvas2");
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
+
+
+const backgroundLayer1 = new Image();
+backgroundLayer1.src = "layer-1.png";
+const backgroundLayer2 = new Image();
+backgroundLayer2.src = "layer-2.png";
+const backgroundLayer3 = new Image();
+backgroundLayer3.src = "layer-3.png";
+const backgroundLayer4 = new Image();
+backgroundLayer4.src = "layer-4.png";
+const backgroundLayer5 = new Image();
+backgroundLayer5.src = "layer-5.png";
+
+window.addEventListener("load", function (e) {
+
+    const slider = document.getElementById("slider");
+    slider.value = gameSpeed;
+    const showGameSpeed = document.getElementById("showGameSpeed");
+    showGameSpeed.innerHTML = gameSpeed;
+    slider.addEventListener("change", function(e) {
+        gameSpeed = e.target.value;
+        showGameSpeed.innerHTML = gameSpeed;
+    })
+
+    class Layer {
+        constructor(image, speedModifier) {
+            this.x = 0;
+            this.y = 0;
+            this.width = 2400;
+            this.height = 700;
+            this.image = image;
+            this.speedModifier = speedModifier;
+            this.speed = gameSpeed * speedModifier;
+        }
+
+        update () {
+            this.speed = gameSpeed * this.speedModifier
+            if(this.x <= -this.width) {
+                this.x = 0;
+            }
+
+            this.x = Math.floor(this.x - this.speed);
+        }
+
+        draw() {
+            ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
+            ctx.drawImage(this.image, this.x + this.width, this.y, this.width, this.height);
+        }
+    }
+
+    const layer1 = new Layer(backgroundLayer1, 0.5);
+    const layer2 = new Layer(backgroundLayer2, 0.7);
+    const layer3 = new Layer(backgroundLayer3, 0.3);
+    const layer4 = new Layer(backgroundLayer4, 1.1);
+    const layer5 = new Layer(backgroundLayer5, 0);
+
+    const gameLayers = [layer1, layer2, layer3, layer4, layer5];
+
+
+    function animate() {
+        ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+
+        gameLayers.forEach((layer) =>  {
+            layer.update();
+            layer.draw();
+        })
+
+        requestAnimationFrame(animate);
+    };
+
+    animate();
+})
